46ffdd06b8e2c6172aa62eed1781fec2b2df3e53
milenavi <43232155+milenavi@users.noreply.github.com>
2019-12-19 23:49:53 +0100

    Delete apiConfig.js

--- src/data/apiConfig.js
+++
@@ -1,3 +0,0 @@
-// You can directly use these constants in the dinnerModel
-export const ENDPOINT = "https://cors-anywhere.herokuapp.com/https://www.googleapis.com/books/v1/volumes";
-export const httpOptions = { headers: {"X-Mashape-Key": "AIzaSyA7mlMd5QEN8zBu52_7-2Hgd9ggUOs1Kjw"} };
