81fb3d757dcc89f4fdbe44ca9a9ef28dbcc6996b
Milena <milenavi@kth.se>
2019-12-31 21:48:18 +0100

    Profile View is included now

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -2,14 +2,12 @@ import React, {Component} from "react";
 import "./Profile.css";
 import modelInstance from "../../data/BookligoModel";
 import { auth } from "../../firebaseConfig/firebaseConfig";
-import {Link} from "react-router-dom";
 
 class Profile extends Component {
     constructor(props) {
         super(props);
         // we put on state the properties we want to use and modify in the component
         this.state = {
-            status: "LOADING",
             user: modelInstance.getCurrentUser()
         };
     }
@@ -20,7 +18,7 @@ class Profile extends Component {
     async componentDidMount() {
         await auth.onAuthStateChanged((user) => {
             if (user) {
-                this.setState({user, status: "LOADED",});
+                this.setState({user});
             }
         });
     }
@@ -29,8 +27,8 @@ class Profile extends Component {
         console.log("User: ", this.state.user);
 
         return (
-            <div className="shopping-cart-dropdown">
-                <div className='app'>
+            <div>
+                <div>
                     {this.state.user ?
                         <div>
                             <div>Name: {this.state.user.displayName}</div>
