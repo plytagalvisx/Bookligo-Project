84f555f4c8a400ea85359f63f56e0467cfcc604f
casse <casse@kth.se>
2020-01-05 23:41:51 +0100

    modified:   Profile.css 	modified:   Profile.js

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -30,10 +30,10 @@ class Profile extends Component {
             <div>
                 <div>
                     {this.state.user ?
-                        <div>
-                            <div>Name: {this.state.user.displayName}</div>
-                            <div>Email: {this.state.user.email}</div>
-                            <img alt="" src={this.state.user.photoURL}/>
+                        <div className = "profile">
+                            <img className = "picture" alt="" src={this.state.user.photoURL}/>
+                            <div className = "name">Name: {this.state.user.displayName}</div>
+                            <div className = "Email">Email: {this.state.user.email}</div>
                         </div>
                         : ""
                     }
